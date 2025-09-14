8dc5f7d71c9fcdd90782d2f1f497995a5574a361
kimdaedan <blueocean.didan@gmail.com>
2025-09-14 11:04:34 +0700

    bagian profile

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -4,9 +4,9 @@ export default function Profile() {
   return (
     <main className="p-6 font-sans">
       <h1 className="text-2xl font-bold">My Profile</h1>
-      <p className="mt-2">Nama: [Isi Nama Anda]</p>
-      <p>NIM: [Isi NIM Anda]</p>
-      <p>Jurusan: [Isi Jurusan]</p>
+      <p className="mt-2">Nama: Didan Naufal Sirodjuddin</p>
+      <p>NIM: 3312411012</p>
+      <p>Jurusan: Teknik Informatika</p>
       {/* Benar: Komponen <Link> juga menggunakan huruf kapital */}
       <Link href="/" className="text-blue-500 hover:underline">Kembali ke Home</Link>
     </main>
